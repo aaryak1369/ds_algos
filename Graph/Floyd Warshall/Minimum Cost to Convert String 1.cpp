e5ac1d0fc280d84788752cdfb8fb338dbe3aaebd
aaryak1369 <joinaarya30@gmail.com>
2024-08-15 14:52:31 +0530

    LC 2967

---
+++ Graph/Floyd Warshall/Minimum Cost to Convert String 1.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void FloydWarshall(vector<vector<long long>> &adjMatrix, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+
+            for(int i = 0; i < original.size(); i++) {
+                int s = original[i] - 'a';
+                int t = changed[i] - 'a';
+
+                adjMatrix[s][t] = min(adjMatrix[s][t], (long long)cost[i]);
+            }
+
+            //Apply Floyd Warshall
+            for (int k = 0; k < 26; ++k) {
+                for (int i = 0; i < 26; ++i) {
+                    for (int j = 0; j < 26; ++j) {
+                        adjMatrix[i][j] = min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j]); 
+                    }
+                }
+            }
+    }
+
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>> adjMatrix(26, vector<long long>(26, INT_MAX));
+
+        FloydWarshall(adjMatrix, original, changed, cost); //update adjMatrix with shortest path using Floyd Warshall
+
+        long long ans = 0;
+
+        for(int i = 0; i < source.length(); i++) {
+            if(source[i] == target[i]) {
+                continue;
+            }
+
+            if(adjMatrix[source[i] -'a'][target[i] - 'a'] == INT_MAX) {
+                return -1;
+            }
+
+            ans += adjMatrix[source[i] -'a'][target[i] - 'a'];
+        }
+
+        return ans;
+
+    }
+};
+
+
+// Driver Code
+int main(){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+
+	int t;
+	cin>>t;
+	// cin.ignore();
+	while(t--){
+		string source;
+        cin>>source;
+
+        string target;
+        cin>>target;
+
+        int n;
+        cin>>n;
+
+        vector<char> original(n);
+        for(int i=0;i<n;i++){
+            cin>>original[i];
+        }
+
+        vector<char> changed(n);    
+        for(int i=0;i<n;i++){
+            cin>>changed[i];
+        }
+
+        vector<int> cost(n);
+        for(int i=0;i<n;i++){
+            cin>>cost[i];
+        }
+        
+
+		Solution obj;
+		long long answer =  obj.minimumCost(source,target,original,changed,cost);
+		cout<<answer<<endl;
+	}
+	return 0;
+}
